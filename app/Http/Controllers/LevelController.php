b044a2c501b21e153679c96504785493c4735aa0
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-29 20:15:42 +0700

    Pertemuan 3 Pratikum 4 Update method  view LevelController and create file view level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -15,7 +15,10 @@ class LevelController extends Controller
         //$row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
         //return 'Update data berhasil. Jumlah data yang diupdate: '.$row.' data';
 
-        $row = Db::delete('delete from m_level where level_kode = ?', ['CUS']); 
-        return 'Delete data berhasil. Jumlah data yang dihapus: '.$row.' data'; 
+        //$row = Db::delete('delete from m_level where level_kode = ?', ['CUS']); 
+        //return 'Delete data berhasil. Jumlah data yang dihapus: '.$row.' data'; 
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }       
 }
