f5f1f8dcdfcc051cfbc35e66cac9af16a727520d
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-29 21:39:01 +0700

    Pertemuan 3 Pratikum 6 Update user nama in UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,9 @@ class UserController extends Controller
     {
         //tambah data user dengan Eloquement Model
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::insert($data); //tambahkan data ke tabel m_user
+        UserModel::where('username', 'customer-1') ->update($data); //update data user
 
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
