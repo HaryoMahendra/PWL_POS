7e7245366ffff976b14e06f5cd26a1558d45f948
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-02-29 21:20:50 +0700

    Pertemuan 3 Pratikum 6 Update UserModel and web routes

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,6 +9,6 @@ class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_users'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'id_user'; //Mendefinisikan primary key dari tabel yang digunakan oleh model ini
+    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan oleh model ini
 }
