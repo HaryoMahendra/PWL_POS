7e0d61d95400a6b522690587977c8043c5cb14f2
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-03-07 13:26:34 +0700

    Pertemuan 4 1.	Praktikum 2.1 – Retrieving Single Models (merubah script UserController)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquement Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); //tambah data ke tabel m_user
-
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        $user = UserModel::find(1); //ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
