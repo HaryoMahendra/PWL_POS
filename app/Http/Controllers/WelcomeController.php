21126689daab00647ac12266dd7c219214d3a115
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-03-28 12:54:41 +0700

    Pertemuan 7 Pratikum 2 membuat WelcomeController

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = [
+            'title' => 'Selamat Datang',
+            'list' => ['Home', 'Welcome']
+        ];
+
+        $activeMenu = 'dashboard';
+
+        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+    }
+}
