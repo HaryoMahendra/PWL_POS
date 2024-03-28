606a27ab9a0355da64a8cceb68233aca1e007e0c
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-03-28 14:01:59 +0700

    Pertemuan 7 Pratikum 3 merubah route web.php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ Route::post('/kategori', [KategoriController::class, 'store']);
 Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
-
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/user', [UserController::class, 'index']);  //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);  //menampilkan data user dalam benttuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']); //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);   //menampilkan detail user    
+    Route::get('/{id}/edit', [UserController::class,'edit']); //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); //menyimppan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+});
