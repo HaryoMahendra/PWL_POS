654e3fd7eebf3b76bd58305abb569d6832fcb090
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-03-28 14:15:54 +0700

    Pertemuan 7 Pratikum 3 merubah UserController dan index.blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+Use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::with('level')->get();
-        return view('user', ['data' => $user]);
+       $breadcrumb = (object) [
+        'title' => 'Daftar User',
+        'list'  => ['Home', 'User']
+       ];
+
+       $page = (object) [
+        'title' => 'Daftar user yang terdaftar dalam sistem'
+       ];
+
+       $activeMenu = 'user'; //set menu yang sedang aktif
+
+       return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'active_menu' => $activeMenu]);
     }
 
     public function tambah()
