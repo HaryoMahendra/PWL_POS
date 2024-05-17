111483987e5aed9854a36eacbb72e0df8f843327
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-05-17 15:06:17 +0700

    Pertemuan 10 Tugas UserController

--- app/Http/Controllers/Api/LevelController.php
+++ app/Http/Controllers/Api/LevelController.php
@@ -1,41 +1,35 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\LevelModel;
 
-class LevelController extends Controller
-{
-    public function index()
-    {
+use App\Http\Controllers\Controller;
+use App\Models\LevelModel;
+use Illuminate\Http\Request;
+
+class LevelController extends Controller {
+    public function index() {
         return LevelModel::all();
     }
 
-    public function store(Request $request)
-    {
+    public function show(LevelModel $level) {
+        return $level;
+    }
+
+    public function store(Request $request) {
         $level = LevelModel::create($request->all());
         return response()->json($level, 201);
     }
 
-    public function show(LevelModel $level)
-    {
-        return LevelModel::find($level);
-    }
-
-    public function update(Request $request, LevelModel $level)
-    {
+    public function update(Request $request, LevelModel $level) {
         $level->update($request->all());
         return LevelModel::find($level);
     }
 
-    public function destroy(LevelModel $user)
-    {
-        $user->delete();
-
+    public function destroy(LevelModel $level) {
+        $level->delete();
         return response()->json([
             'success' => true,
-            'message' => 'Data Terhapus',
+            'message' => 'Data terhapus'
         ]);
     }
 }
